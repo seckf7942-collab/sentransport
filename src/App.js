fc0cadc3887cbd29f0b016386034d54bac71663e
Fatou Seck <seckf7942@gmail.com>
2026-04-20 11:16:12 +0000

    Exercices Lab 1 : Footer, date Header, Statistique

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
+import Statistique from './Statistique';
+
 function App() {
-return (
-<div className="App">
-<Header />
-<main className="contenu">
-<p>Bienvenue ! Cette application vous aide a trouver
-votre ligne de bus a Dakar.</p>
-</main>
-</div>
-);
+  return (
+    <div className="App">
+      <Header />
+      <main className="contenu">
+        <p>Bienvenue ! Cette application vous aide à trouver
+        votre ligne de bus à Dakar.</p>
+        <div className="stats-container">
+          <Statistique />
+          <Statistique />
+          <Statistique />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
 export default App;
---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">10</span>
+      <span className="stat-libelle">Lignes</span>
+    </div>
+  );
+}
+
+export default Statistique;
